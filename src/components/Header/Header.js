0f705ebcdb6fe7b7f7accf9667c360a87e79860f
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-03 13:14:36 +0600

    main file and Home ,Header,OrderReview com. created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+
+            <h1>Welcome to TShirt Mania</h1>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+
+            <h1>This is Order Review</h1>
+
+        </div>
+    );
+};
+
+export default OrderReview;
